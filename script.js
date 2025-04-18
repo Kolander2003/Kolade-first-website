a64582619a0d45e6066d85620138d8e4837101e6
Kolander2003 <kfjinadu@gmail.com>
2025-04-17 20:53:51 -0600

    Commit

---
+++ script.js
@@ -0,0 +1,32 @@
+/*
+document: refers to the current document
+get elementbyid: it gets the element based on id value
+ src: tells you the source
+ funtion: saves your code for later
+ onclick:
+*/
+
+
+document.getElementById("SOM").onclick = function(){
+    document.getElementById("displayimage").src = "images/SOM.jpeg";
+
+}
+document.getElementById("Music").onclick = function(){
+    document.getElementById("displayimage").src = "images/music.jpeg";
+
+}
+
+document.getElementById("Roses").onclick = function(){
+    document.getElementById("displayimage").src = "images/roses.jpeg";
+
+}
+
+document.getElementById("Games").onclick = function(){
+    document.getElementById("displayimage").src = "images/Games.jpeg";
+
+}
+
+document.getElementById("Family").onclick = function(){
+    document.getElementById("displayimage").src = "images/Familybonding.jpeg";
+
+}
